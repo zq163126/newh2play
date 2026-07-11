3ddf1af5ce32a0c18ed2061e9c1fe5a6f85a322e
zq163126 <zq163126@outlook.com>
2026-07-11 20:31:40 +0800

    Update browser.py

--- browser.py
+++ browser.py
@@ -4,9 +4,11 @@ from pathlib import Path
 from typing import Optional
 from dotenv import load_dotenv
 from patchright.sync_api import BrowserContext, Playwright
- os.environ['https_proxy'] = 'socks5://127.0.0.1:1080'
- os.environ['http_proxy'] = 'socks5://127.0.0.1:1080'
+# os.environ['https_proxy'] = 'socks5://127.0.0.1:1080'
+# os.environ['http_proxy'] = 'socks5://127.0.0.1:1080'
 # 在程序启动时设置代理，仅在未设置环境变量时生效
+http_proxy: socks5://127.0.0.1:1080
+https_proxy: socks5://127.0.0.1:1080
 
 
 load_dotenv()
